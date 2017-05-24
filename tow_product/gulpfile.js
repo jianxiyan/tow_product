de8a5cf9eb23fcc45f6112a831afbbc592245886
jian xi yan <415883681@qq.com>
2017-05-24 11:29:38 +0800

    new tow_product

---
+++ tow_product/gulpfile.js
@@ -0,0 +1,19 @@
+ var gulp = require('gulp');
+ var connect= require('gulp-connect'); 
+ 
+gulp.task('copy-index',function(){ 	
+	return gulp.src('index.html').pipe(gulp.dest('dist')).pipe(connect.reload());
+ });
+
+gulp.task('sever',function(){ 
+	connect.server({
+		root:'dist',
+		livereload:true 
+		});
+}) 
+
+gulp.task('watch',function(){ 
+	gulp.watch('index.html',['copy-index']); 
+})
+
+gulp.task('default',['sever','watch']); 
